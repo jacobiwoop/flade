fab72940aa38fa0eeb69d8c06ec26b561d240ef4
jacobiwoop <jacobiwoop@gmail.com>
2025-06-17 01:35:37 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Ajouter le token ngrok si la variable est définie (version ngrok 2.x)
 if [ -n "$NGROK_AUTHTOKEN" ]; then
-    ngrok authtoken "$NGROK_AUTHTOKEN"
+    ngrok config add-authtoken "$NGROK_AUTHTOKEN"
 fi
 
 # Lancer le serveur websocket (écoute sur le port 8081) en arrière-plan
