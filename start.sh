f33e993df1a67d8c466692c05cf83c458ae4917e
jacobiwoop <jacobiwoop@gmail.com>
2025-06-17 01:16:32 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# Lancer le serveur WebSocket Ratchet sur le port 8081 en arrière-plan
+# Enregistrer le token ngrok s'il est défini
+# if [ -n "$NGROK_AUTHTOKEN" ]; then
+ngrok config add-authtoken 2c8bOYFGPf7xyfMFMLnMFFq1LCN_2Yoc8Q5eD6JSpqLwihRE2
+# fi
+
+# Lancer le serveur websocket en arrière-plan
 php websocket/server.php &
 
-# Lancer Apache en premier plan (obligatoire pour Docker)
+# Lancer ngrok TCP sur le port 8081 en arrière-plan
+ ngrok http --url=seahorse-eager-joey.ngrok-free.app 8080 --log=stdout &
+
+# Démarrer Apache en premier plan (obligatoire pour Docker)
 apache2-foreground
