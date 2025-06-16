1b055e3d476af9b221ac7b5dd3347942455e3bf2
jacobiwoop <jacobiwoop@gmail.com>
2025-06-16 23:32:40 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Démarrer Apache en arrière-plan
+apache2-foreground &
+
+# Démarrer le serveur WebSocket (par exemple Ratchet)
+php server/websocket-server.php &
+
+# Garder le container vivant en attendant les processus
+wait -n
+
+# Si un des processus meurt, on stoppe le container
+exit $?
